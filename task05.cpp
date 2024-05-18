3efe2cba0d0af83b4b61e3c9d7a9fe36c8c344f7
leevermy <risnotr@gmail.com>
2024-05-18 11:14:10 +0300

    Done some changes in 5th task

--- task05.cpp
+++ task05.cpp
@@ -19,7 +19,7 @@ int main() {
 
     while (player_points < 6 && audience_points < 6) {
         int offset;
-        std::cout << "\nEnter the offset (displacement) for rotating the top: ";
+        std::cout << "\n= NEW ROUND =\nEnter the offset (displacement) for rotating the top: ";
         std::cin >> offset;
 
         sector = (sector + offset) % total_sectors;
@@ -56,7 +56,7 @@ int main() {
             std::cout << "Your answer is correct. You got 1 point" << std::endl;
             ++player_points;
         } else {
-            std::cout << "Wrong. Audience got 1 point" << std::endl;
+            std::cout << "Wrong. The correct answer is \"" << answer << "\". Audience got 1 point" << std::endl;
             ++audience_points;
         }
     }
@@ -76,14 +76,25 @@ std::string ReadFile(const std::filesystem::path & file_path) {
         std::cerr << "Unable to open file: " << file_path << std::endl;
         return "";
     }
-    char buffer [1024];
+
     std::string content = "";
-    while(!file.eof()) {
-        file.read(buffer, sizeof(buffer) - 1);
-        buffer[file.gcount()] = '\0';
-        std::string line(buffer);
-        content += line;
+    file.seekg(0, std::ios::end);
+    content.resize(file.tellg());
+    file.seekg(0, std::ios::beg);
+    file.read(&content[0], content.size());
+
+    if (!file && !file.eof()) {
+        std::cerr << "Error occurred while reading the file." << std::endl;
     }
+
+    // char buffer [1024];
+    // std::string content = "";
+    // while(!file.eof()) {
+    //     file.read(buffer, sizeof(buffer) - 1);
+    //     buffer[file.gcount()] = '\0';
+    //     std::string line(buffer);
+    //     content += line;
+    // }
     file.close();
     return content;
 }
